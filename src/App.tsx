9ab4365a40b39d04e57f3ee05f3feb249537219e
Archana Ojha <t3ojar01@students.oamk.fi>
2025-02-19 18:22:06 +0200

    initial commit: set up React app with TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Typography} from '@mui/material';
+
+function App() {
+  return (
+    <>
+     <Typography variant='h1'>Hello, welcome to my react app.....</Typography>
+      
+    </>
+  )
+}
+
+export default App;
